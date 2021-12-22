9f64edad644188c9cb092fad2808a6aea4ce5e14
Jon Principe <jon@rocketinsights.com>
2021-12-22 14:43:29 -0500

    add docs for Theme

--- lib/tailwind_live_components/theme.ex
+++ lib/tailwind_live_components/theme.ex
@@ -1,4 +1,35 @@
 defmodule TailwindLiveComponents.Theme do
+  @moduledoc """
+  Theme struct containing different Tailwind classes
+
+  ```
+  %TailwindLiveComponents.Theme{
+    # standard colors
+    bg_color: "bg-white",
+    light_bg_color: "bg-gray-200",
+    border_color: "border-gray-300",
+    shadow_color: "",
+    text_color: "text-gray-900",
+    light_text_color: "text-gray-500",
+    lighter_text_color: "text-gray-300",
+    error_text_color: "text-red-800",
+
+    # selected colors
+    selected_bg_color: "bg-sky-900/75",
+    selected_border_color: "border-transparent",
+    selected_shadow_color: "shadow-sky-900/50",
+    selected_text_color: "text-white",
+    selected_light_text_color: "text-sky-200",
+    selected_highlight_text_color: "text-sky-900",
+
+    # focus colors
+    focus_bg_color: "bg-sky-900",
+    focus_ring_color: "ring-sky-900",
+    focus_border_color: "border-sky-900"
+  }
+  ```
+  """
+
   defstruct [
     # standard colors
     bg_color: "bg-white",
@@ -8,7 +39,7 @@ defmodule TailwindLiveComponents.Theme do
     text_color: "text-gray-900",
     light_text_color: "text-gray-500",
     lighter_text_color: "text-gray-300",
-    error_text_color: "text-red-500",
+    error_text_color: "text-red-800",
 
     # selected colors
     selected_bg_color: "bg-sky-900/75",
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule TailwindLiveComponents.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/sourceflo/tailwind_live_components"
-  @version "0.1.2"
+  @version "0.1.3"
 
   def project do
     [
