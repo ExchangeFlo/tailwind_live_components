f32ff3a50c0ccff9611ea3aa51c5d8d5934a1df2
Jon Principe <jon@rocketinsights.com>
2022-01-03 08:38:33 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule TailwindLiveComponents.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/ExchangeFlo/tailwind_live_components"
-  @version "0.2.1"
+  @version "0.2.2"
 
   def project do
     [
